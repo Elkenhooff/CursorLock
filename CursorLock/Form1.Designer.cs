562132e52759e6311b0b6ce224c4dc23d742b323
Pedro David <146982890+Elkenhooff@users.noreply.github.com>
2024-03-10 00:34:10 -0300

    Initial commit

---
+++ CursorLock/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace CursorLock
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(180, 14);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Size = new Size(136, 56);
+            button1.TabIndex = 0;
+            button1.Text = "Posicionar Cursor";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(163, 97);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(171, 18);
+            label1.TabIndex = 1;
+            label1.Text = "CTRL + F8 = Start/Stop";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 18F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(494, 124);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Font = new Font("Arial", 12F);
+            Margin = new Padding(4);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CursorLock";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+    }
+}
---
+++ CursorLock/Form1.cs
@@ -0,0 +1,71 @@
+using NonInvasiveKeyboardHookLibrary;
+namespace CursorLock
+{
+    public partial class Form1 : Form
+    {
+        int i = 1;
+        bool ativado = false;
+        int y;
+        private KeyboardHookManager keyboardHookManager;
+        public Form1()
+        {
+            InitializeComponent();
+            keyboardHookManager = new KeyboardHookManager();
+            keyboardHookManager.Start();
+            keyboardHookManager.RegisterHotkey(NonInvasiveKeyboardHookLibrary.ModifierKeys.Control, (int)Keys.F8, codigoPrincipal);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            y = -1;
+            this.WindowState = FormWindowState.Maximized;
+            this.BackColor = Color.White;
+            this.FormBorderStyle = FormBorderStyle.None;
+            button1.Visible = false;
+            label1.Visible = false;
+            this.Opacity = 0.01;
+
+            this.MouseClick += selecionarTrava;
+        }
+
+        private void selecionarTrava(object sender, MouseEventArgs e)
+        {
+            this.MouseClick -= selecionarTrava;
+            Point posicao = PointToClient(Cursor.Position);
+            y = posicao.Y;
+            MessageBox.Show(y.ToString());
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+            button1.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            this.Opacity = 1;
+            label1.Visible = true;
+        }
+        private void codigoPrincipal()
+        {
+
+            ativado = !ativado;
+            if (ativado)
+            {
+                i = 0;
+                this.Invoke((MethodInvoker)delegate
+                {
+                    this.WindowState = FormWindowState.Minimized;
+                });
+                while (i == 0)
+                {
+                    int x = Cursor.Position.X;
+                    Cursor.Position = new Point(x, y);
+                    Thread.Sleep(20);
+                }
+            }
+            else
+            {
+                i = 1;
+                this.Invoke((MethodInvoker)delegate
+                {
+                    this.WindowState = FormWindowState.Normal;
+                });
+            }
+        }
+    }
+}
